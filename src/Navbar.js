4d551de3ea716e1edcab7722288dd701df2e2310
Serg <serg27makar@gmail.com>
2023-06-15 10:54:59 +0300

    Clined code.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {Text, View, StyleSheet, Button} from "react-native";
 import {Translator} from "./utils/js/main";
 import {useDispatch, useSelector} from "react-redux";
