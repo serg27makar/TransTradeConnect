c5d277bc4efc20d4928a67fd45a517cbe5cd5fc7
Serg <serg27makar@gmail.com>
2023-06-16 15:53:43 +0300

    Added star rating.

--- src/components/StarsRating.js
+++ src/components/StarsRating.js
@@ -1,33 +1,36 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import {Star} from "./Star";
 import * as Progress from 'react-native-progress';
 
-export const StarRating = () => {
+export const StarRating = ({point, rating, label}) => {
     const [pointState, setPointState] = useState([])
-    const starRating = 35
-    const point = 2
-    // const result = Math.round((data.expense / data.income) * 100)
-
     const points = () => {
         let likes = [];
-        for (let i = 0; i < 5; i++) {
-            likes.push(point <= i)
+        for (let i = 1; i <= 5; i++) {
+            const isStar = point >= i
+            likes.push(isStar)
         }
         setPointState(likes)
     }
     useEffect(() => {
         points();
-    }, [])
+    }, [point])
 
     return (
-        <View style={styles.block}>
-            {pointState.map(i => {
-                return (<Star rating={i}/>)
-            })}
-           <View style={styles.progressBar}>
-               <Progress.Bar progress={0.3} height={10} width={50} />
-           </View>
+        <View>
+            <Text style={styles.label}>{label}</Text>
+            <View style={styles.block}>
+                {pointState.map((i, index) => {
+                    return (<Star rating={i} key={index}/>)
+                })}
+                <View style={styles.progressBarWrapper}>
+                    <View style={styles.progressBar}>
+                        <Progress.Bar progress={rating} height={10} width={100} />
+                    </View>
+                    <Text>{rating * 100}%</Text>
+                </View>
+            </View>
         </View>
     );
 }
@@ -37,7 +40,16 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
     },
+    progressBarWrapper: {
+        height: 25,
+        flexDirection: "row",
+        padding: 4,
+    },
     progressBar: {
-        height: 10,
+        height: 5,
+        margin: 5,
+    },
+    label: {
+        paddingLeft: 10
     }
 })
