d9ae7a4d3046e24939fe7ac1c6a1285fbfec68e7
Serg <serg27makar@gmail.com>
2023-09-26 19:09:22 +0300

    Added rushen translate.

--- src/assets/langs/ru.js
+++ src/assets/langs/ru.js
@@ -1,3 +1,75 @@
 export const ru = {
-    logo: "Trans Trade Connect ru"
+    logo: "Trans Trade Connect",
+
+    dispatcher: "Диспетчер",
+    carrier: "Перевозчик",
+    trader: "Трейдер",
+
+    Password: "Введите пароль",
+    Registration: "Регистрация",
+    Login: "Вход",
+
+    NotPhoneNumber: "Это не похоже на номер телефона",
+    TypePhoneNumber: "Введите номер телефона",
+    EnterPhoneWithoutSpaces: "Введите номер телефона без пробелов и специальных символов",
+    CheckOut: "Проверить",
+    ToAdd: "Добавить",
+    EnterSearchNumber: "Введите здесь поисковый номер",
+
+    TotalRating: "Общий рейтинг доверия",
+    MoreDetailedInformation: "Более детально",
+
+    DispatcherRating1: "предоставляет не точную информацию",
+    DispatcherRating2: "большенство информации не соответствует действительности",
+    DispatcherRating3: "информация требует дополнительной проверки",
+    DispatcherRating4: "большенство информации правдива",
+    DispatcherRating5: "предоставляет точную информацию",
+
+    CarrierRating1: "абсолютно не надежный",
+    CarrierRating2: "часто бывают неприятности",
+    CarrierRating3: "возможны неприятности",
+    CarrierRating4: "почти все нормально",
+    CarrierRating5: "ответственный и надежный",
+
+    Calculation: "Расчет",
+    InformativenessUnloading: "Информационность разгрузки",
+    Downtime: "Время простоя",
+
+    CalcRating1: "не расчитывается",
+    CalcRating2: "с большой задержкой",
+    CalcRating3: "на продолжении месяца",
+    CalcRating4: "на продолжении недели",
+    CalcRating5: "после разгрузки",
+
+    InfoRating1: "не предоставил ни какой информации",
+    InfoRating2: "почти не предоставил информации",
+    InfoRating3: "предоставил мало или не точную информацию",
+    InfoRating4: "предоставил почти всю информацию",
+    InfoRating5: "предоставил абсолютно точную информацию",
+
+    DownRating1: "постоянные простои",
+    DownRating2: "много простоев",
+    DownRating3: "бывают простои",
+    DownRating4: "редкие простои",
+    DownRating5: "практически без простоев",
+
+    selectUserType: "Выберите тип пользователя",
+    aboutWhomYouWantToShare: "Про кого ви хотите поделится",
+    byNumber: "По номеру",
+    rate: "виберите один из ответов",
+
+    enterNameOrganization: "Введите имя или название организации",
+    fieldRequired: "Это поле обязательное",
+    ratingRequired: "выберите наиболее правильный ответ",
+    save: "Сохранить",
+    somethingWentWrong: "Что-то пошло не так, попробуйте пожалуйста позже",
+    successfullyAdded: "Информация успешно добавлена",
+
+    editPhoneNumber: "Редактировать номер телефона",
+    addPhoneNumber: "Добавить номер телефона",
+    addedPhoneNumber: "Добавленный номер",
+    edit: "Редактировать",
+    add: "Добавить",
+
+    NoOneWasFound: "По этому номеру никого не нашли",
 }
